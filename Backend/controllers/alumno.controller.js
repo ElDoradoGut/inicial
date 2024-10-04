447f32d3b67490ee3fd053105414f2b01b91af49
Andrés Dorado <andresdoradog@gmail.com>
2024-10-03 22:34:03 -0600

    corrected error

--- Backend/controllers/alumno.controller.js
+++ Backend/controllers/alumno.controller.js
@@ -1,11 +1,40 @@
 import { modeloAlumnos } from "../models/alumno.model.js";
 
 modeloAlumnos.create({
-    name:"Andrés",
-    apepat:"Dorado",
-    edd:31
+    name: "Andrés",
+    apepat: "Dorado",
+    apemat: "Gutiérrez",
+    edad: 31
 })
 
-export const test=()=>{
+modeloAlumnos.create({
+    name: "Pedro",
+    apepat: "Pedroso",
+    apemat: "Perrón",
+    edad: 25
+})
+
+modeloAlumnos.create({
+    name: "Tony",
+    apepat: "Tigre",
+    apemat: "Tigre",
+    edad: 23
+})
+
+modeloAlumnos.create({
+    name: "Tim",
+    apepat: "Marín",
+    apemat: "Dopingüe",
+    edad: 29
+})
+
+modeloAlumnos.create({
+    name: "Solferino",
+    apepat: "Picudo",
+    apemat: "Picudo",
+    edad: 22
+})
+
+export const test = () => {
     console.log("Se esta mandando llamar correctamente el controlador.")
 }
